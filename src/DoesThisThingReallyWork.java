3fc88d6c338eacac3da9b97dcbbfe19ae55144ba
Mette <Mette@192.168.0.158>
2021-02-17 11:27:08 +0100

    We try again Beeches

--- src/DoesThisThingReallyWork.java
+++ src/DoesThisThingReallyWork.java
@@ -3,7 +3,6 @@ public class DoesThisThingReallyWork {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Does this thiiing really work?");
+		System.out.print("Does this thiiiiiiiing really work?");
 	}
-
 }
