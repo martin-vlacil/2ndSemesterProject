d7a93a96bb662863d6a725b0114201d0d0d7cc45
Mette <Mette@192.168.0.158>
2021-02-17 11:26:46 +0100

    Class created to test if this works

---
+++ src/DoesThisThingReallyWork.java
@@ -0,0 +1,9 @@
+
+public class DoesThisThingReallyWork {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Does this thiiing really work?");
+	}
+
+}
