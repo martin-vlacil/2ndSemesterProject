cce6014e0103706fa9b2005c86053ce9f2c6dfd1
martinko-boop <75024378+martinko-boop@users.noreply.github.com>
2021-05-05 13:02:03 +0200

    throws exception idk

--- src/databaseLayer/BookingDBIF.java
+++ src/databaseLayer/BookingDBIF.java
@@ -1,10 +1,11 @@
 package databaseLayer;
 
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 
 public interface BookingDBIF
 {
 	boolean create(String title, String description, String contactName, String phoneNumber,
 					String email, LocalDateTime startTime, LocalDateTime endTime, 
-					int numberOfParticipants);
+					int numberOfParticipants) throws SQLException;
 }
--- src/databaseLayer/LogEntryDBIF.java
+++ src/databaseLayer/LogEntryDBIF.java
@@ -1,10 +1,11 @@
 package databaseLayer;
 
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 
 import modelLayer.LogEntry;
 
 public interface LogEntryDBIF
 {
-	LogEntry create(String action, LocalDateTime time);
+	LogEntry create(String action, LocalDateTime time) throws SQLException;
 }
