7ed8eb1643ee4214836da222b7780dc9b773a61c
WrexBG <40688355+WrexBG@users.noreply.github.com>
2021-05-12 15:28:47 +0300

    CreateBookingDialog changes

--- src/uiLayer/CreateBookingDialog.java
+++ src/uiLayer/CreateBookingDialog.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
@@ -62,18 +63,17 @@ public class CreateBookingDialog extends JDialog {
 		setBounds(100, 100, 783, 502);
 		
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPanel.setBackground(new Color(40,41,82));
+		contentPanel.setBackground(new Color(240, 240, 240));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		GridBagLayout gbl_contentPanel = new GridBagLayout();
 		gbl_contentPanel.columnWidths = new int[]{0, 0};
-		gbl_contentPanel.rowHeights = new int[]{0, 0, 0};
+		gbl_contentPanel.rowHeights = new int[]{43, 0, 0};
 		gbl_contentPanel.columnWeights = new double[]{1.0, 1.0};
 		gbl_contentPanel.rowWeights = new double[]{0.0, 1.0};
 		contentPanel.setLayout(gbl_contentPanel);
 		{
 			JPanel titlePanel = new JPanel();
-			titlePanel.setBackground(new Color(214, 65, 65));
+			titlePanel.setBackground(new Color(40, 41, 82));
 			GridBagConstraints gbc_titlePanel = new GridBagConstraints();
 			gbc_titlePanel.gridwidth = 2;
 			gbc_titlePanel.insets = new Insets(0, 0, 10, 0);
@@ -100,7 +100,7 @@ public class CreateBookingDialog extends JDialog {
 		}
 		{
 			JPanel leftPanel = new JPanel();
-			leftPanel.setBackground(new Color(255, 255, 255));
+			leftPanel.setBackground(Color.WHITE);
 			GridBagConstraints gbc_leftPanel = new GridBagConstraints();
 			gbc_leftPanel.insets = new Insets(0, 20, 0, 0);
 			gbc_leftPanel.fill = GridBagConstraints.BOTH;
@@ -108,25 +108,26 @@ public class CreateBookingDialog extends JDialog {
 			gbc_leftPanel.gridy = 1;
 			contentPanel.add(leftPanel, gbc_leftPanel);
 			GridBagLayout gbl_leftPanel = new GridBagLayout();
-			gbl_leftPanel.columnWidths = new int[]{0};
-			gbl_leftPanel.rowHeights = new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-			gbl_leftPanel.columnWeights = new double[]{0.0};
-			gbl_leftPanel.rowWeights = new double[]{0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,Double.MIN_VALUE};
+			gbl_leftPanel.columnWidths = new int[]{30, 0, 60};
+			gbl_leftPanel.rowHeights = new int[]{30, 0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+			gbl_leftPanel.columnWeights = new double[]{0.0, 1.0, 0.0};
+			gbl_leftPanel.rowWeights = new double[]{0.0, 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,Double.MIN_VALUE};
 			leftPanel.setLayout(gbl_leftPanel);
 			{
 				JLabel titleLabel = new JLabel("Event Title*");
 				GridBagConstraints gbc_titleLabel = new GridBagConstraints();
-				gbc_titleLabel.insets = new Insets(0, 0, 5, 0);
-				gbc_titleLabel.gridx = 0;
+				gbc_titleLabel.anchor = GridBagConstraints.WEST;
+				gbc_titleLabel.insets = new Insets(0, 0, 5, 5);
+				gbc_titleLabel.gridx = 1;
 				gbc_titleLabel.gridy = 0;
 				leftPanel.add(titleLabel, gbc_titleLabel);
 			}
 			{
 				titleTextField = new JTextField();
 				GridBagConstraints gbc_titleTextField = new GridBagConstraints();
-				gbc_titleTextField.insets = new Insets(0, 0, 5, 0);
+				gbc_titleTextField.insets = new Insets(0, 0, 5, 5);
 				gbc_titleTextField.fill = GridBagConstraints.HORIZONTAL;
-				gbc_titleTextField.gridx = 0;
+				gbc_titleTextField.gridx = 1;
 				gbc_titleTextField.gridy = 1;
 				leftPanel.add(titleTextField, gbc_titleTextField);
 				titleTextField.setColumns(10);
@@ -134,17 +135,17 @@ public class CreateBookingDialog extends JDialog {
 			{
 				JLabel organizationLabel = new JLabel("Organization");
 				GridBagConstraints gbc_organizationLabel = new GridBagConstraints();
-				gbc_organizationLabel.insets = new Insets(0, 0, 5, 0);
-				gbc_organizationLabel.gridx = 0;
+				gbc_organizationLabel.insets = new Insets(0, 0, 5, 5);
+				gbc_organizationLabel.gridx = 1;
 				gbc_organizationLabel.gridy = 2;
 				leftPanel.add(organizationLabel, gbc_organizationLabel);
 			}
 			{
 				organizationDropDownPlaceholder = new JTextField();
 				GridBagConstraints gbc_organizationDropDownPlaceholder = new GridBagConstraints();
-				gbc_organizationDropDownPlaceholder.insets = new Insets(0, 0, 5, 0);
+				gbc_organizationDropDownPlaceholder.insets = new Insets(0, 0, 5, 5);
 				gbc_organizationDropDownPlaceholder.fill = GridBagConstraints.HORIZONTAL;
-				gbc_organizationDropDownPlaceholder.gridx = 0;
+				gbc_organizationDropDownPlaceholder.gridx = 1;
 				gbc_organizationDropDownPlaceholder.gridy = 3;
 				leftPanel.add(organizationDropDownPlaceholder, gbc_organizationDropDownPlaceholder);
 				organizationDropDownPlaceholder.setColumns(10);
@@ -152,17 +153,17 @@ public class CreateBookingDialog extends JDialog {
 			{
 				JLabel attendeesLabel = new JLabel("Number of attendees*");
 				GridBagConstraints gbc_attendeesLabel = new GridBagConstraints();
-				gbc_attendeesLabel.insets = new Insets(0, 0, 5, 0);
-				gbc_attendeesLabel.gridx = 0;
+				gbc_attendeesLabel.insets = new Insets(0, 0, 5, 5);
+				gbc_attendeesLabel.gridx = 1;
 				gbc_attendeesLabel.gridy = 4;
 				leftPanel.add(attendeesLabel, gbc_attendeesLabel);
 			}
 			{
 				textField_2 = new JTextField();
 				GridBagConstraints gbc_textField_2 = new GridBagConstraints();
-				gbc_textField_2.insets = new Insets(0, 0, 5, 0);
+				gbc_textField_2.insets = new Insets(0, 0, 5, 5);
 				gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
-				gbc_textField_2.gridx = 0;
+				gbc_textField_2.gridx = 1;
 				gbc_textField_2.gridy = 5;
 				leftPanel.add(textField_2, gbc_textField_2);
 				textField_2.setColumns(10);
@@ -171,25 +172,25 @@ public class CreateBookingDialog extends JDialog {
 				JLabel contactLabel = new JLabel("Contact Person Information");
 				contactLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
 				GridBagConstraints gbc_contactLabel = new GridBagConstraints();
-				gbc_contactLabel.insets = new Insets(0, 0, 5, 0);
-				gbc_contactLabel.gridx = 0;
+				gbc_contactLabel.insets = new Insets(0, 0, 5, 5);
+				gbc_contactLabel.gridx = 1;
 				gbc_contactLabel.gridy = 6;
 				leftPanel.add(contactLabel, gbc_contactLabel);
 			}
 			{
 				JLabel nameLabel = new JLabel("First- and Last Name*");
 				GridBagConstraints gbc_nameLabel = new GridBagConstraints();
-				gbc_nameLabel.insets = new Insets(0, 0, 5, 0);
-				gbc_nameLabel.gridx = 0;
+				gbc_nameLabel.insets = new Insets(0, 0, 5, 5);
+				gbc_nameLabel.gridx = 1;
 				gbc_nameLabel.gridy = 7;
 				leftPanel.add(nameLabel, gbc_nameLabel);
 			}
 			{
 				nameTextField = new JTextField();
 				GridBagConstraints gbc_nameTextField = new GridBagConstraints();
-				gbc_nameTextField.insets = new Insets(0, 0, 5, 0);
+				gbc_nameTextField.insets = new Insets(0, 0, 5, 5);
 				gbc_nameTextField.fill = GridBagConstraints.HORIZONTAL;
-				gbc_nameTextField.gridx = 0;
+				gbc_nameTextField.gridx = 1;
 				gbc_nameTextField.gridy = 8;
 				leftPanel.add(nameTextField, gbc_nameTextField);
 				nameTextField.setColumns(10);
@@ -197,17 +198,17 @@ public class CreateBookingDialog extends JDialog {
 			{
 				JLabel phoneLabel = new JLabel("Phone number");
 				GridBagConstraints gbc_phoneLabel = new GridBagConstraints();
-				gbc_phoneLabel.insets = new Insets(0, 0, 5, 0);
-				gbc_phoneLabel.gridx = 0;
+				gbc_phoneLabel.insets = new Insets(0, 0, 5, 5);
+				gbc_phoneLabel.gridx = 1;
 				gbc_phoneLabel.gridy = 9;
 				leftPanel.add(phoneLabel, gbc_phoneLabel);
 			}
 			{
 				phoneTextField = new JTextField();
 				GridBagConstraints gbc_phoneTextField = new GridBagConstraints();
-				gbc_phoneTextField.insets = new Insets(0, 0, 5, 0);
+				gbc_phoneTextField.insets = new Insets(0, 0, 5, 5);
 				gbc_phoneTextField.fill = GridBagConstraints.HORIZONTAL;
-				gbc_phoneTextField.gridx = 0;
+				gbc_phoneTextField.gridx = 1;
 				gbc_phoneTextField.gridy = 10;
 				leftPanel.add(phoneTextField, gbc_phoneTextField);
 				phoneTextField.setColumns(10);
@@ -215,17 +216,17 @@ public class CreateBookingDialog extends JDialog {
 			{
 				JLabel emailLabel = new JLabel("Email");
 				GridBagConstraints gbc_emailLabel = new GridBagConstraints();
-				gbc_emailLabel.insets = new Insets(0, 0, 5, 0);
-				gbc_emailLabel.gridx = 0;
+				gbc_emailLabel.insets = new Insets(0, 0, 5, 5);
+				gbc_emailLabel.gridx = 1;
 				gbc_emailLabel.gridy = 11;
 				leftPanel.add(emailLabel, gbc_emailLabel);
 			}
 			{
 				emailTextField = new JTextField();
 				GridBagConstraints gbc_emailTextField = new GridBagConstraints();
-				gbc_emailTextField.insets = new Insets(0, 0, 5, 0);
+				gbc_emailTextField.insets = new Insets(0, 0, 0, 5);
 				gbc_emailTextField.fill = GridBagConstraints.HORIZONTAL;
-				gbc_emailTextField.gridx = 0;
+				gbc_emailTextField.gridx = 1;
 				gbc_emailTextField.gridy = 12;
 				leftPanel.add(emailTextField, gbc_emailTextField);
 				emailTextField.setColumns(10);
@@ -234,7 +235,7 @@ public class CreateBookingDialog extends JDialog {
 		{
 			JPanel rightPanel = new JPanel();
 			rightPanel.setPreferredSize(new Dimension(100,100));
-			rightPanel.setBackground(new Color(255, 255, 255));
+			rightPanel.setBackground(Color.WHITE);
 			GridBagConstraints gbc_rightPanel = new GridBagConstraints();
 			gbc_rightPanel.insets = new Insets(0, 0, 0, 20);
 			gbc_rightPanel.fill = GridBagConstraints.BOTH;
@@ -350,7 +351,7 @@ public class CreateBookingDialog extends JDialog {
 			}
 			{
 				JLabel errorMessageRoom = new JLabel("Room not available");
-				errorMessageRoom.setForeground(new Color(255, 0, 255));
+				errorMessageRoom.setForeground(Color.RED);
 				errorMessageRoom.setBackground(new Color(255, 255, 255));
 				GridBagConstraints gbc_errorMessageRoom = new GridBagConstraints();
 				gbc_errorMessageRoom.insets = new Insets(0, 0, 0, 5);
@@ -362,14 +363,19 @@ public class CreateBookingDialog extends JDialog {
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			buttonPane.setBackground(new Color(40,41,82));
+			buttonPane.setBackground(new Color(240, 240, 240));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
 				JButton saveButton = new JButton("Save");
-				saveButton.setForeground(new Color(255, 255, 255));
-				saveButton.setBackground(new Color(9,158,105));
+				saveButton.setForeground(Color.WHITE);
+				saveButton.setBackground(new Color(86, 197, 104));
+				saveButton.setBorder(new EmptyBorder(5, 30, 5, 30));
+				saveButton.setFocusable(false);
+				saveButton.setFont(new Font("Roboto", Font.BOLD, 15));
+				
 				saveButton.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
+						//TODO create booking
 					}
 				});
 				saveButton.setActionCommand("Save");
@@ -378,7 +384,12 @@ public class CreateBookingDialog extends JDialog {
 			}
 			{
 				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setBackground(new Color(192, 192, 192));
+				cancelButton.setForeground(new Color(149, 149, 149));
+				cancelButton.setBackground(new Color(234, 234, 238));
+				cancelButton.setBorder(new EmptyBorder(5, 30, 5, 30));
+				cancelButton.setFocusable(false);
+				cancelButton.setFont(new Font("Roboto", Font.BOLD, 15));
+	
 				cancelButton.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						dispose();
