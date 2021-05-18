86bdfe57fedce42b550a67d3ca0b021b748cfb4f
martinko-boop <75024378+martinko-boop@users.noreply.github.com>
2021-05-18 13:13:11 +0200

    Update TestBookingController.java

--- src/testLayer/TestBookingController.java
+++ src/testLayer/TestBookingController.java
@@ -31,8 +31,11 @@ class TestBookingController
 	RoomController roomCtr;
 	BookingDBStub bookingDBStub;
 	
-	User user1;
-	User user2;
+	LocalDateTime startTime;
+	LocalDateTime endTime;
+	
+	User shortUser;
+	User longUser;
 	ArrayList<Room> selectedRooms;
 	
 
@@ -51,7 +54,11 @@ class TestBookingController
 		roomCtr = new RoomController();
 		bookingDBStub = new BookingDBStub();
 		
-		user1 = new User(1, "Ib", "Ib@gmail.com", "+4512345678", "Marketing Manager", UserType.DEFAULT, new Organization(1, "IKEA"));
+		startTime = LocalDateTime.of(2021, 6, 5, 15, 0);
+		endTime = LocalDateTime.of(2021, 6, 5, 23, 0);
+		
+		shortUser = new User(1, "Ib", "Ib@gmail.com", "+4512345678", "Marketing Manager", UserType.DEFAULT, new Organization(1, "IKEA"));
+		longUser = new User(2, "ForSomeReasonVeryLongName", "forTestingPurposesWeHaveCreatedThisVeryLongEmailThatContainsExactlyOneHundredCharactersUwU@gmail.com", "+451234567891234", "Managing Marketer", UserType.DEFAULT, new Organization(2, "Bauhaus"));
 		selectedRooms = new ArrayList<>();
 	}
 	
@@ -60,18 +67,16 @@ class TestBookingController
 	{
 	}
 
-	// CB1 - Successful booking
+	// CB1 - Successful booking - TC1
 	@Test
-	void shouldCreateBookingAndSaveToDatabaseWithCorrectInfo() throws SQLException
+	void shouldCreateBookingAndSaveToDatabaseWithCorrectShortInfo() throws SQLException
 	{
 		//Arrange
-		LocalDateTime startTime = LocalDateTime.of(2021, 6, 5, 15, 0);
-		LocalDateTime endTime = LocalDateTime.of(2021, 6, 5, 23, 0);
 		selectedRooms.add(roomCtr.findByID(1));
 		bookingCtr.setStub(bookingDBStub);
 		
 		//Act
-		boolean isBookingCreated = bookingCtr.confirmBooking("2nd Semester Exam", "We are all passing :)", null, 15, user1, selectedRooms, startTime, endTime);
+		boolean isBookingCreated = bookingCtr.confirmBooking("2nd Semester Exam", "We are all passing :)", null, 15, shortUser, selectedRooms, startTime, endTime);
 		
 		//Assert
 		assertTrue(isBookingCreated, "Controller returned False");
@@ -80,7 +85,30 @@ class TestBookingController
 		
 		assertTrue(startTime.isEqual(booking.getStartTime()), "Start time doesn't match");
 		assertTrue(endTime.isEqual(booking.getEndTime()), "End time doesn't match");
-		assertEquals(user1, booking.getCreatedBy(), "User doesn't match");
+		assertEquals(shortUser, booking.getCreatedBy(), "User doesn't match");
+		assertEquals(selectedRooms.get(0), booking.getRoom(), "Room doesn't match");
+		assertEquals(15, booking.getNumberOfParticipants(),"Number of participants doesn't match");
+	}
+	
+	//CB1 - Successful booking - TC2
+	@Test
+	void shouldCreateBookingAndSaveToDatabaseWithCorrectLongInfo() throws SQLException
+	{
+		//Arrange
+		selectedRooms.add(roomCtr.findByID(1));
+		bookingCtr.setStub(bookingDBStub);
+		
+		//Act
+		boolean isBookingCreated = bookingCtr.confirmBooking("2nd Semester Exam", "We are all passing :)", null, 15, longUser, selectedRooms, startTime, endTime);
+		
+		//Assert
+		assertTrue(isBookingCreated, "Controller returned False");
+		
+		Booking booking = bookingDBStub.getBooking();
+		
+		assertTrue(startTime.isEqual(booking.getStartTime()), "Start time doesn't match");
+		assertTrue(endTime.isEqual(booking.getEndTime()), "End time doesn't match");
+		assertEquals(longUser, booking.getCreatedBy(), "User doesn't match");
 		assertEquals(selectedRooms.get(0), booking.getRoom(), "Room doesn't match");
 		assertEquals(15, booking.getNumberOfParticipants(),"Number of participants doesn't match");
 	}
