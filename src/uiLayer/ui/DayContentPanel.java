35824ba5c4cdd04588232050afd5218a00c125af
TMSKHL <2000tamas@gmail.com>
2021-05-20 13:02:05 +0200

    unused functionality removed

--- src/uiLayer/ui/DayContentPanel.java
+++ src/uiLayer/ui/DayContentPanel.java
@@ -342,8 +342,6 @@ public class DayContentPanel extends JPanel {
                 */
             	//XXX added functionality for displaying only events for a certain room
             	JCalendar calendar = this.owner.getOwner();
-            	if ((calendar.getRoom() == null) || (calendar.getRoom().getId() == event.getRoom().getId()))
-            	{
 	                Color bgColor = event.getType().getBackgroundColor();
 	                bgColor = bgColor == null ? config
 	                        .getEventDefaultBackgroundColor() : bgColor;
@@ -400,8 +398,8 @@ public class DayContentPanel extends JPanel {
             	}
             }
         }
-    }
-  //XXX CalendarEvent changed to Booking, changed name to getEvent from GetNonMonthEvent as month events are displayed differently in the legacy code.
+  
+    //XXX CalendarEvent changed to Booking, changed name to getEvent from GetNonMonthEvent as month events are displayed differently in the legacy code.
     private Booking getEvent(final int x, final int y) {
     	
     	//XXX Collection creation moved to custom method
